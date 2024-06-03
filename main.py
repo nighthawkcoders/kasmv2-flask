d6ab097b8f42fd720a62c7a4389fc3d51802f8e1
tanishapatil1234 <tanishapatil07@gmail.com>
2024-06-03 05:48:37 -0700

    addition of admin routes

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ db=SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 Session(app)
 
+# -------------- classes ----------------
+
 # User Class
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,13 +25,21 @@ class User(db.Model):
     classes = db.Column(db.String(255))  
     kasm_server_needed = db.Column(db.Boolean, default=False)
     password = db.Column(db.String(255), nullable=False)
-    status = db.Column(db.Integer,default=1, nullable=False)
+    status = db.Column(db.Integer,default=0, nullable=False)
 
     def __repr__(self):
         return f'User("{self.id}","{self.fname}","{self.ghid}","{self.email}","{self.classes}","{self.kasm_server_needed}","{self.status}")'
 
+# Admin Class 
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(255), nullable=False)
+    classes = db.Column(db.String(255))
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
-
+    def __repr__(self):
+        return f'Admin("{self.id}","{self.fname}","{self.email}","{self.classes}")'
 
 # main index 
 @app.route('/') 
@@ -37,11 +47,89 @@ def index():
     return render_template('/index.html', 
     title="")
 
+# -------------- admin area ----------------
+
+# add admins once and manually 
+def add_admin():
+    db.create_all()
+    # Check if the admin already exists
+    existing_admin = Admin.query.filter_by(email='jmortensen@powayusd.com').first()
+    if not existing_admin:
+        admin = Admin(
+            fname="John Mortensen",
+            password=bcrypt.generate_password_hash('jmort123').decode('utf-8'),
+            email='jmortensen@powayusd.com',
+            classes='CSP,CSA'
+        )
+        db.session.add(admin)
+        db.session.commit()
+
 # admin login
-@app.route('/admin/') 
+@app.route('/admin/', methods=["POST", "GET"]) 
 def adminIndex():
-    return render_template('admin/index.html', 
-    title="Admin Login")
+    # check request is post or get
+    if request.method == "POST": 
+        #get value of field 
+        email = request.form.get('email')
+        password = request.form.get('pwd')
+        # check for empty vals
+        if email=="" and password == "": 
+            flash("Please fill all the fields", 'danger')
+            return redirect("/admin/")
+        else: 
+            # get admins by username
+            admins = Admin().query.filter_by(email=email).first()
+            if admins and bcrypt.check_password_hash(admins.password, password):
+                session['admins_id'] = admins.id
+                session['admins_email'] = admins.email
+                flash('Admin Login Successful', 'success')
+                return redirect('/admin/dashboard')
+            else:
+                flash('Incorrect Email or Password', 'success')
+                return redirect('/admin/')
+    else:
+        return render_template('admin/index.html', 
+        title="Admin Login")
+
+# admin Dashboard 
+@app.route("/admin/dashboard")
+def adminDashboard():
+    if not session.get('admins_id'):
+        flash('Please log in first', 'danger')
+        return redirect('/admin/')
+    totalUser = User.query.count()
+    totalApproved = User.query.filter_by(status=1).count()
+    totalUnapproved = User.query.filter_by(status=0).count()
+    return render_template('admin/dashboard.html', title="Admin Dashboard", total=totalUser, totalA = totalApproved, totalU = totalUnapproved)
+
+# admin Logout 
+@app.route("/admin/logout")
+def adminLogout():
+    session.clear()
+    return redirect("/")
+
+# admin get all users
+@app.route('/admin/get-all-user', methods=["POST", "GET"])
+def adminGetAllUser():
+    admin_id = session.get('admins_id')
+    admin = Admin.query.get(admin_id)
+    admin_classes = admin.classes.split(',')
+
+    all_users = User.query.all()
+    
+    # Filter users to match any of the admin's classes
+    filtered_users = [user for user in all_users if any(cls in user.classes.split(',') for cls in admin_classes)]
+    return render_template('admin/all-user.html', title = "Get All Users", users=filtered_users)
+
+# approve users
+@app.route('/admin/approve-user/<int:id>')
+def adminApprove(id):
+    User().query.filter_by(id=id).update(dict(status=1))
+    db.session.commit()
+    flash('Approval Succesful', 'success')
+    return redirect('/admin/get-all-user')
+
+
 
 # -------------- user area ----------------
 
@@ -120,7 +208,6 @@ def userSignup():
         return render_template('user/signup.html', 
         title="User Signup")
 
-
 # user dashboard
 @app.route("/user/dashboard")
 def userDashboard():
@@ -134,13 +221,16 @@ def userDashboard():
         flash('Please log in first', 'danger')
         return redirect('/user/')
 
-    return render_template('user/dashboard.html', title="User Dashboard", name=user_name)
+    if session.get('user_id'):
+        id = session.get('user_id')
+    users = User().query.filter_by(id=id).first()
+    return render_template('user/dashboard.html', title="User Dashboard", name=user_name, users=users)
 
 # user logout 
 @app.route('/user/logout')
 def userLogout():
     session.clear()
-    return redirect("/user/")
+    return redirect("/")
     
 # user change pwd 
 @app.route('/user/change-password', methods=["POST", "GET"])
@@ -175,7 +265,37 @@ def userChangePassword():
     else:
         return render_template('user/change-password.html', title="Change Password", name=user_name)
 
+# user update info
+@app.route('/user/update-profile', methods=["POST", "GET"])
+def userUpdateProfile():
+    if not session.get('user_id'):
+        return redirect('/user/')
+    id = session.get('user_id')
+    users = User.query.get(id)
+
+    if request.method == "POST":
+        fname = request.form.get('fname')
+        ghid = request.form.get('ghid')
+        email = request.form.get('email')
+        classes = request.form.getlist('classes')
+        kasm_server_needed = True if 'kasm_server_needed' in request.form else False
+        if fname == "" or ghid == "" or email == "" : 
+             flash('Please fill all the fields', 'danger')
+             return redirect('/user/update-profile')
+        else:
+            classes_str = ','.join(classes)
+            User.query.filter_by(id=id).update(dict(fname=fname,ghid=ghid, email=email, classes=classes_str, kasm_server_needed = kasm_server_needed))
+            db.session.commit()
+            flash('Profile updated successfully', 'success')
+            return redirect('/user/dashboard')
+    else:
+        return render_template('user/update-profile.html', title="Update Profile", users=users)
+
+
+
 if __name__ == "__main__":
     with app.app_context():
+        # comment admin line out after first run
+        add_admin()
         db.create_all()
     app.run(debug=True)
